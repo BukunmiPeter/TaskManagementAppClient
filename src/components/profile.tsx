49711573d84a6953aeb121d8577b34f1749488e6
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2023-04-10 19:46:37 +0100

    ...

---
+++ src/components/profile.tsx
@@ -0,0 +1,7 @@
+import React, { FC, ReactElement } from 'react';
+
+// export const Profile: FC =(): ReactElement=>{}
+
+export const Profile: FC = (): ReactElement => {
+  return <h2>Profile</h2>;
+};
